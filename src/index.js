64f1129e03f56a25660e162742e8319263ec4bf0
lionel-nj <lionel@mobilitydata.org>
2021-05-17 12:19:56 -0400

    fix paths

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 
 
-import { aggregate, merge, removeDirectories } from '/Users/lionel/IdeaProjects/metrics/src/scripts/AggregateData.js'
-import { fetchRawData } from '/Users/lionel/IdeaProjects/metrics/src/scripts/FetchRawData.js'
+import { aggregate, merge, removeDirectories } from './scripts/AggregateData.js'
+import { fetchRawData } from './scripts/FetchRawData.js'
 
 const DATA = 'data'
 const RAW = 'raw'
