f358fb6050b6e32ce9d8bab25479e1757a22c2dc
lionel-nj <lionel@mobilitydata.org>
2021-05-26 14:51:19 -0400

    try something else

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ const TOKEN_PATH = 'token.json'
 
 // Load client secrets from a secrets
 // Authorize a client with credentials, then call the Google Sheets API.
-authorize(JSON.parse(CREDENTIALS), updateGoogleSheet)
-console.log(JSON.parse(CREDENTIALS))
+authorize(updateGoogleSheet)
+
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -27,10 +27,13 @@ console.log(JSON.parse(CREDENTIALS))
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize (credentials, callback) {
+function authorize (callback) {
+  const credentials = JSON.parse(CREDENTIALS)
   const client_id = credentials.client_id
   const client_secret = credentials.client_secret
-  const redirect_uris = credentials['redirect_uris']
+  const redirect_uris = credentials.redirect_uris
+  console.log(client_id)
+  console.log(client_secret)
   console.log(redirect_uris)
   const oAuth2Client = new google.auth.OAuth2(
     client_id, client_secret, redirect_uris[0])
