ac99d7bf568511977c452052d548e3bc197e65b7
lionel-nj <lionel@mobilitydata.org>
2021-05-26 14:54:56 -0400

    .installed

--- index.js
+++ index.js
@@ -2,24 +2,14 @@ import { createRequire } from 'module'
 import { updateGoogleSheet } from './src/scripts/GoogleSheetUpdater.js'
 
 const require = createRequire(import.meta.url)
-const fs = require('fs')
-const readline = require('readline')
 const { google } = require('googleapis')
 require('dotenv').config()
 export const CREDENTIALS = process.env.CREDENTIALS
 export const TOKEN = process.env.TOKEN
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json'
-
 // Load client secrets from a secrets
 // Authorize a client with credentials, then call the Google Sheets API.
-authorize(updateGoogleSheet)
-
+authorize(JSON.parse(CREDENTIALS), updateGoogleSheet)
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -27,11 +17,10 @@ authorize(updateGoogleSheet)
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize (callback) {
-  const credentials = JSON.parse(CREDENTIALS)
-  const client_id = credentials.client_id
-  const client_secret = credentials.client_secret
-  const redirect_uris = credentials.redirect_uris
+function authorize (credentials, callback) {
+  const client_id = credentials.installed.client_id
+  const client_secret = credentials.installed.client_secret
+  const redirect_uris = credentials.installed.redirect_uris
   console.log(client_id)
   console.log(client_secret)
   console.log(redirect_uris)
@@ -40,40 +29,4 @@ function authorize (callback) {
 
   oAuth2Client.setCredentials(JSON.parse(TOKEN))
   callback(oAuth2Client)
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken (oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  })
-  console.log('Authorize this app by visiting this url:', authUrl)
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  })
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close()
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) {
-        return console.error(
-          'Error while trying to retrieve access token', err)
-      }
-      oAuth2Client.setCredentials(token)
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) {
-          return console.error(err)
-        }
-        console.log('Token stored to', TOKEN_PATH)
-      })
-      callback(oAuth2Client)
-    })
-  })
 }
